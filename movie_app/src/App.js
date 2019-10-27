98afc2ae39a0157a5a44eea88e4a746923ee72f8
Q3333 <hil02047@gmail.com>
2019-10-27 18:54:12 +0900

    3.3 영화앱 들어가기 전

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -2,37 +2,18 @@ import React from "react";
 
 class App extends React.Component {
   state = {
-    count: 0
-  };
-
-  constructor(props) {
-    super(props);
-    console.log("hello");
-  }
-  add = () => {
-    this.setState(current => ({ count: current.count + 1 }));
-  };
-  minus = () => {
-    this.setState(current => ({ count: current.count - 1 }));
+    isLoading: true,
+    movies: []
   };
 
   componentDidMount() {
-    console.log("component rendered");
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
   }
-
-  componentDidUpdate() {
-    console.log("i just updated");
-  }
-
   render() {
-    console.log("i`m rendering");
-    return (
-      <div>
-        <h1> The number is : {this.state.count} </h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading" : "we are ready"}</div>;
   }
 }
 
